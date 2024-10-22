6d6ecfa35cfe43501905c21085279e6d64d677aa
Jyoti Sing <jyotising@Jyotis-MacBook-Pro.local>
2024-10-22 20:24:13 +0530

    main.go edit

--- main.go
+++ main.go
@@ -27,3 +27,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Icon not found!")
 	}
 }
+
+// Main function to start the server
+func main() {
+	http.HandleFunc("/", Handler)
+	// Start the server on port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
